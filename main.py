712c0d496925f75f6926410ab5db3074f3a93d05
zll <1507297170@qq.com>
2022-03-19 17:46:31 +0800

    19376449

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-#main.py
 import os
 os.system('python function_19376449.py')
-=======
-import os
-os.system('python function_20373313.py')
->>>>>>> 7426b840953ba408ace4d52649da39228cc69406
+
